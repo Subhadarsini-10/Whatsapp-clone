ab9a68cf24a4e6128f950ba472b16138ead24ac7
Subhadarsini Pattnaik <71827873+Subhadarsini-10@users.noreply.github.com>
2022-05-27 23:53:19 +0530

    Add files via upload

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import { Avatar } from "@mui/material";
+import "./Sidebar.css";
+import {
+  ChatBubbleOutline,
+  DonutLargeOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
 
 function Sidebar() {
   return (
-    <div className='Sidebar'>
-        <h1>Sidebar</h1>
+    <div className="Sidebar">
+      <div className="sidebar_header">
+        <Avatar />
+        <div className="sidebar_headerRight">
+          <DonutLargeOutlined />
+          <ChatBubbleOutline />
+          <MoreHorizOutlined />
+        </div>
+      </div>
+      <div className="sidebar_search"></div>
+      <div className="sidebar_chats"></div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
